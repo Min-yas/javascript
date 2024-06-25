3062654621e18fd153e7be291c2704eaae6d2884
Yasmina K <yasminakhatun2404@gmail.com>
2024-06-26 00:30:15 +0530

    Update q9.js

--- q9.js
+++ q9.js
@@ -1,3 +1,5 @@
+/9. Write a function that removes the third element from an array and returns a new array with the removed element. Do not modify the original array.  
+
 let arr = [9 , 65 , 34 , 23 , 32 , 56];
 
 function remove_element(num){
