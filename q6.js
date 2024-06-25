3acc50797fc126f2160929f9b1b126c603610891
Yasmina K <yasminakhatun2404@gmail.com>
2024-06-26 00:28:03 +0530

    Update q6.js

--- q6.js
+++ q6.js
@@ -1,3 +1,5 @@
+//6. Write a function that takes an array of numbers and returns an object with two properties: sum, the sum of all the numbers, and product, the product of all the numbers using reduce method.
+
 let num = [3,5,2]
 
 function sumofall(sum , num){
