41750786484d43981cfa17b95a4c79b17116b67d
Yasmina K <yasminakhatun2404@gmail.com>
2024-06-26 00:29:40 +0530

    Update q8.js

--- q8.js
+++ q8.js
@@ -1,3 +1,6 @@
+/8. Given an array of numbers, write a function that returns the index of the first number that is greater than 10. If no such number exists, return -1.  
+
+
 let arr = [9 , 33 , 34 , 23 , 45 , 56];
 
 let x = arr.length
