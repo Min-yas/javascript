ff8d7de7d97dd813e0e8b547989e36e927b11fda
Yasmina K <yasminakhatun2404@gmail.com>
2024-06-26 00:26:54 +0530

    Update q5.js

--- q5.js
+++ q5.js
@@ -1,10 +1,6 @@
 //Given an array of objects representing users with name and age properties, write a function that returns an array of names of users who are over18.
-const users = [
-  { name: 'Alice', age: 22 },
-  { name: 'Bob', age: 17 },
-  { name: 'Charlie', age: 19 },
-  { name: 'David', age: 15 }];
-==>
+//const users = [{ name: 'Alice', age: 22 },{ name: 'Bob', age: 17 },{ name: 'Charlie', age: 19 },{ name: 'David', age: 15 }];
+
 const users = [
       { name: 'Alice', age: 22 },
       { name: 'Bob', age: 17 },
